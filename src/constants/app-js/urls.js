6375921086c4a1ea89a91858dbbcea1ba5979e3e
GKP <GKP@Ganesh-MacBook-Air.local>
2025-12-10 23:25:50 +0530

    feat: setup axios

---
+++ src/constants/app-js/urls.js
@@ -0,0 +1,7 @@
+const URLS = {
+    BASE_URL: "https://apis.allsoft.co/api",
+
+    GET_OTP: "/documentManagement//generateOTP",
+    VERIFY_OTP: "/documentManagement//validateOTP",
+  };
+  export default URLS;
