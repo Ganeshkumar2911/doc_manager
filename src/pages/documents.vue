046dc0c2b27267dc736bcf3a503083a78ca6e0cb
GKP <GKP@Ganesh-MacBook-Air.local>
2025-12-11 04:09:28 +0530

    feat: list page ui & api integrations

--- src/pages/documents.vue
+++ src/pages/documents.vue
@@ -14,6 +14,18 @@
                 </v-btn>
             </v-col>
         </v-row>
+        <v-row>
+            <v-col cols="12">
+                <v-data-table
+                    :headers="headers"
+                    :items="documentList"
+                    :items-per-page="10"
+                    hide-default-footer
+                    :header-props="{class: 'bg-indigo text-white'}"
+                    
+                ></v-data-table>
+            </v-col>
+        </v-row>
         <UploadDocument ref="uploadDocument" />
     </div>
 </template>
@@ -27,12 +39,95 @@ export default {
     },
     data() {
         return {
-            isUploading: false
+            isUploading: false,
+            headers: [
+                { 
+                    title: 'Document ID', 
+                    key: 'document_id', 
+                    align: 'start',
+                    sortable: true 
+                },
+                { 
+                    title: 'Category', 
+                    key: 'major_head', 
+                    align: 'start',
+                    sortable: true 
+                },
+                { 
+                    title: 'Sub-Category', 
+                    key: 'minor_head', 
+                    align: 'start',
+                    sortable: true 
+                },
+                { 
+                    title: 'Document Date', 
+                    key: 'document_date', 
+                    align: 'start',
+                    sortable: true 
+                },
+                { 
+                    title: 'Remarks', 
+                    key: 'document_remarks', 
+                    align: 'start',
+                    sortable: false 
+                },
+                { 
+                    title: 'Upload Time', 
+                    key: 'upload_time', 
+                    align: 'start',
+                    sortable: true 
+                },
+                { 
+                    title: 'Uploaded By', 
+                    key: 'uploaded_by', 
+                    align: 'start',
+                    sortable: true 
+                },
+                { 
+                    title: 'Actions', 
+                    key: 'actions', 
+                    align: 'center',
+                    sortable: false 
+                }
+            ],
+            documentList: []
         }
+    },
+    mounted() {
+        this.getDocumentList();
     },
     methods: {
         addDocument() {
             this.$refs.uploadDocument.openDialog();
+        },
+        getDocumentList() {
+            const formData = {
+                uploaded_by: sessionStorage.getItem("user_id"),
+                major_head: "",
+                minor_head: "",
+                from_date: "",
+                to_date: "",
+                tags: [],
+                start: 0,
+                length: 10,
+                filterId: "",
+                search: {
+                    value: "",
+                }
+
+            }
+            const successHandler = (response) => {
+                this.documentList = response.data;
+            }
+            const failureHandler = (error) => {
+                console.error('Failed to get document list:', error);
+            }
+            this.$api.request_POST(
+                this.$urls.GET_DOCUMENT_LIST,
+                formData,
+                successHandler,
+                failureHandler
+            );
         }
     }
 }
